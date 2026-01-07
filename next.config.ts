1f8862e3cb0a4c9dd64fe1f34d5fef8dde41e36f
ZomJeedz <onenarok777@gmail.com>
2026-01-08 00:51:32 +0700

    feat: Configure remote image patterns for Google Drive and set server actions body size limit.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,28 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "doc-0s-4s-docs.googleusercontent.com",
+      },
+    ],
+  },
+
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
 };
 
 export default nextConfig;
